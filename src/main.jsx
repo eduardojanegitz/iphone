0e7d92e6c20c0bd5dccc9de592a4b0a3eeb25b02
Eduardo Alves <edu0alves00@gmail.com>
2024-06-27 18:42:40 -0300

    feat: Instalando e configurando sentry para monitoramento

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import * as Sentry from "@sentry/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+Sentry.init({
+  dsn: "https://98cf3983a3f71cac3f6f10b4b43bbd55@o4507494854230016.ingest.us.sentry.io/4507494856654848",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
